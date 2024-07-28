bbff0736c6b2014b538c1ee5c6eff3151af724cc
Okasha Habib <160734391+okashabib-oh@users.noreply.github.com>
2024-07-28 21:53:36 +0500

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,6 @@ onAuthStateChanged(auth, (user) => {
     if(user){
         window.location = '../pages/dashboard.html'
     }else{
-        window.location = '../pages/login.html';
+        window.location = './pages/login.html';
     }
 })
