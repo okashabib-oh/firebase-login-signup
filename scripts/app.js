7fa7aef979656945108bcb8be2171e82594d476f
Okasha Habib <okasha.qavitech786@gmail.com>
2024-07-28 22:32:52 +0500

    changes

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,9 @@
 import { auth, onAuthStateChanged } from "./firebase.js";
 
 onAuthStateChanged(auth, (user) => {
-    if(user){
-        window.location = '../pages/dashboard.html'
-    }else{
-        window.location = '/pages/login.html';
+    if (user) {
+        window.location = 'pages/dashboard.html'
+    } else {
+        window.location = 'pages/login.html';
     }
 })
